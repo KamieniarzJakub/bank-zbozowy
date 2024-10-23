430a1441746c989c54c704277769509399d0ad98
KamieniarzJakub <jakub.kamieniarz@student.put.poznan.pl>
2024-10-23 22:19:07 +0200

    Coś poszło nie tak

---
+++ 04-git/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  int a = 11;
+  int x = 5;
+  int z = 6;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println("Witaj świecie!");
+    System.out.println("Hello world!");
+  }
+}
